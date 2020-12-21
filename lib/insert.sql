37f38f073825b4cb3acb4615e2893b67fdcdf426
Christerle Nounoute <christerlesn@gmail.com>
2020-12-21 06:19:39 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,11 +1,3 @@
--- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 7, "male", "black", "calm", "true");
--- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 27, "male", "panda", "angry", "false");
--- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 3, "female", "panda", "calm", "false");
--- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 15, "male", "black", "angry", "true");
--- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 6, "female", "white", "confused", "false");
--- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 1, "male", "black", "angry", "true");
--- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 21, "female", "white", "calm", "false");
--- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 18, "male", "black", "angry", "true");
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
 (1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
 (2,"Rowdy", 10, "M", "black", "intense", 1),
